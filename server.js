3d4c81bd4bed61c5873396602cdbd2b52fde03d5
Jonathan Huynh <jonhuynh05@gmail.com>
2019-10-25 15:19:28 -0700

    install bcrypt

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const app = express();
 const methodOverride = require("method-override");
 const bodyParser = require("body-parser");
 const session = require("express-session");
+const Contractor = require("./models/contractors");
+const Reviewer = require("./models/reviewers");
+const bcrypt = require("bcryptjs");
 const PORT = 5000;
 require("./db/db")
 
@@ -30,7 +33,32 @@ app.get("/login", (req, res) => {
 // REGISTER ROUTE
 
 app.get("/register", (req, res) => {
-    res.render("registration.ejs")
+    res.render("registration.ejs", {
+        duplicate: req.session.duplicate
+    })
+})
+
+app.post("/register", async (req, res) => {
+    try{
+        const foundContractor = await Contractor.findOne({email: req.body.email});
+        const foundReviewer = await Reviewer.findOne({email: req.body.email});
+        if(foundContractor || foundReviewer){
+            req.session.duplicate = "Email already exists. Please try another.";
+            res.redirect("/register");
+        }
+        else if(req.body.contractor === true){
+            const password = req.body.password;
+            const passwordHash = 
+            Contractor.create(req.body);
+            req.session.username = req.body.username;
+
+            res.redirect("/");
+        }
+    }
+    catch (err) {
+        res.send(err)
+        console.log(err)
+    }
 })
 
 
