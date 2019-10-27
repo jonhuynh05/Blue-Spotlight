cee0a5ac9f97acd67fecd2620d213ca320d02a76
Jonathan Huynh <jonhuynh05@gmail.com>
2019-10-27 15:36:22 -0700

    add reviewer show and edit pages

--- controller/reviewer.js
+++ controller/reviewer.js
@@ -3,9 +3,10 @@ const router = express.Router();
 const Reviewer = require("../models/reviewers");
 const bcrypt = require("bcryptjs");
 
-router.get("/", (req, res) => {
+router.get("/", async (req, res) => {
     try{
-        const loggedInReviewer = Reviewer.findOne({username: req.session.username});
+        console.log(req.session)
+        const loggedInReviewer = await Reviewer.findOne({username: req.session.username});
         res.render("reviewers/index.ejs", {
             reviewer: loggedInReviewer
         })
@@ -13,6 +14,33 @@ router.get("/", (req, res) => {
     catch(err){
         res.send(err);
         console.log(err);
+    }
+})
+
+router.get("/:id", async (req, res) => {
+    try{
+        const loggedInReviewer = await Reviewer.findOne({username: req.session.username});
+        res.render("reviewers/show.ejs", {
+            reviewer: loggedInReviewer
+        })
+    }
+    catch(err){
+        res.send(err)
+        console.log(err)
+    }
+})
+
+router.get("/:id/edit", async (req, res) => {
+    try{
+        const loggedInReviewer = await Reviewer.findOne({username: req.session.username});
+        res.render("reviewers/edit.ejs", {
+            reviewer: loggedInReviewer,
+            duplicate: req.session.duplicate
+        })
+    }
+    catch(err){
+        res.send(err)
+        console.log(err)
     }
 })
 
