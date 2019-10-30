527669b0e717c239fb61073656befed51258295e
Jonathan Huynh <jonhuynh05@gmail.com>
2019-10-30 09:16:14 -0700

    finish contractor delete route

--- controller/contractor.js
+++ controller/contractor.js
@@ -196,7 +196,7 @@ router.delete("/:id", async (req, res) => {
     try{
         const contractorToBeDeleted = await Contractor.findOne({username: req.session.username})
         for (let i = 0; i < contractorToBeDeleted.reviews.length; i++){
-            Review.findByIdAndDelete(contractorToBeDeleted.reviews[i]);
+            await Review.findByIdAndDelete(contractorToBeDeleted.reviews[i]);
             let foundContractor = await Contractor.findOne({writtenReviews: contractorToBeDeleted.reviews[i]});
             if(foundContractor){
                 foundContractor.writtenReviews.remove(contractorToBeDeleted.reviews[i]);
@@ -209,7 +209,7 @@ router.delete("/:id", async (req, res) => {
             }
         }
         for (let i = 0; i < contractorToBeDeleted.writtenReviews.length; i++){
-            Review.findByIdAndDelete(contractorToBeDeleted.writtenReviews[i])
+            await Review.findByIdAndDelete(contractorToBeDeleted.writtenReviews[i])
             let foundContractor = await Contractor.findOne({reviews: contractorToBeDeleted.writtenReviews[i]});
             foundContractor.reviews.remove(contractorToBeDeleted.writtenReviews[i]);
             foundContractor.save()
